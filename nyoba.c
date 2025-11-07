888b0b125d0d4b412af33d1d14a1eccd6eff7a64
Ahmad Damanhuri <daman@mhs.usk.ac.id>
2025-11-07 16:08:30 +0700

    Telah diubah ASA BEMON

--- nyoba.c
+++ nyoba.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 int main () {
-    int a, b;
+    int a, b, c;
     printf("Suka suka dia lah, malas aing");
+    scanf("%d",&a);
     printf("Masukkan angka: ");
+    scanf("%d",&b);
     scanf("Masukkan angka: ");
+    scanf("%d",&c);
     return 0;
 }
